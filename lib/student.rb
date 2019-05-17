6f3df5beaaa82424f8a3a7ec3e5027777a1ddce4
April <fanmeng0417@gmail.com>
2019-05-17 10:11:24 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -2,8 +2,8 @@ require_relative "./user.rb"
 require "pry"
 class Student < User
   attr_accessor :knowledge
-  def initialize
-    @knowledge = []
+  def initialize (knowledge = [])
+    @knowledge = knowledge
   end
   def learn(knowledge)
     @knowledge << knowledge
